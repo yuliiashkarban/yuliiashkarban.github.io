5de0b3b6a2f7acd8dd09614c4b60318baf520cb1
Rostyslav Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2020-05-21 21:31:59 +0200

    Updates

---
+++ precache-manifest.fe93c7676040f8cab6e4c294e225bc6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa7cdd693ddf18b084e8010d970bc7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a36f880cc5c1f0da719",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "299b8b50408aea45152d",
+    "url": "/static/js/2.35735744.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35735744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a36f880cc5c1f0da719",
+    "url": "/static/js/main.0d6f2bc7.chunk.js"
+  },
+  {
+    "revision": "4610ed3e5797c4db3eac",
+    "url": "/static/js/runtime-main.28d5734e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
