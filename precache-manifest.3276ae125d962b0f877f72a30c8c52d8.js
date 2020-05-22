37c518e6cc7ba0057c3213f7f30008e825a2ec83
Rostyslav Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2020-05-23 00:03:47 +0200

    Updates

--- precache-manifest.3276ae125d962b0f877f72a30c8c52d8.js
+++ precache-manifest.3276ae125d962b0f877f72a30c8c52d8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0201157c34f5bcf496882d4ab9e8361a",
+    "revision": "18397227c27727e8da038d45c0516556",
     "url": "/index.html"
   },
   {
-    "revision": "cd259efb8f32605f1fee",
+    "revision": "2d70f5edb54370ed4c86",
     "url": "/static/css/main.aa571012.chunk.css"
   },
   {
-    "revision": "e7801a98d7a74ddcdf5a",
-    "url": "/static/js/2.000b72cd.chunk.js"
+    "revision": "8abbaf078b80b2955b01",
+    "url": "/static/js/2.19de46d2.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.000b72cd.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.19de46d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cd259efb8f32605f1fee",
-    "url": "/static/js/main.e994ca95.chunk.js"
+    "revision": "2d70f5edb54370ed4c86",
+    "url": "/static/js/main.73ebdefe.chunk.js"
   },
   {
     "revision": "4610ed3e5797c4db3eac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d1e2ef84284f0ce1b0814e41ecbd990.js"
+  "/precache-manifest.3276ae125d962b0f877f72a30c8c52d8.js"
 );
 
 self.addEventListener('message', (event) => {
