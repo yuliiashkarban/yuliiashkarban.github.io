bcd5273906b462b358ca368530e0d4eae90842a5
Rostyslav Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2020-05-28 20:28:40 +0200

    Some changes in dashboard text.

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
         <div className="photo">
           <img src="./profile_photo.jpg" alt="profile"></img>
         </div>
-        <div className="short-info"><p>Hi,</p> My name is Yuliia, I am really happy you are here.<p>I am a student with a creative soul who have a huge dream to succeed in graphic design industry. And here you’ll find everything you need to know about me and my steps to gain what I want. My portfolio, my thoughts and some inspiration you will find here.So, prepare your tea, sweets and be ready to learn something about me.</p>So, prepare your tea, sweets and be ready to learn something about me.</div>
+        <div className="short-info"><p>Hi,</p> My name is Yuliia and I am really happy you are here.<p>I am a student with a creative soul, who has a huge dream to succeed in graphic design industry. And here you’ll find everything you need to know about me and my steps I made to gain what I want. My portfolio, my thoughts and some inspiration you will find here.</p>So, prepare your tea, sweets and be ready to learn something about me.</div>
         <div className="portfolio-best-description">This is short description about my portfolio</div>
         <div className="portfolio-best">Some portfolio photoes goes here</div>
     </div>
